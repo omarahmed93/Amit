fb87d82d1743fb84b364046558912b26f863cb01
Omar ahmed <75174169+Omaroz93@users.noreply.github.com>
2022-03-05 19:05:18 +0200

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # cmp common number function work in python 2 only does not work in py3
+
+# In[35]:
+
+
+
+a = 1 
+b = 2 
+print(cmp(a, b))  
+  
+a = 2
+b = 2 
+print(cmp(a, b))  
+   
+a = 3
+b = 2 
+print(cmp(a, b))
+
+
+
+# # sqrt of 4
+
+# In[16]:
+
+
+import math
+print(math.sqrt(4))
+
+
+# # The natural logarithm
+
+# In[22]:
+
+
+print(math.log(14))
+
+
+# In[ ]:
+
+
+
+
